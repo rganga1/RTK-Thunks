7940870e7d458431086ecce38f56f323ab9ef872
RakeshG <rockingrakeshshetty@gmail.com>
2023-05-21 12:42:19 +0530

    019_L361._Rendering_the_List_of_Users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -14,14 +14,24 @@ function UsersList() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <Skeleton times={6} />;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
